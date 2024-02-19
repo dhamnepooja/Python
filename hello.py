bc9acc9e7bcc0007ac7e2591cfe6a527e682d08a
dhamnepooja <poojadhamne84@gmail.com>
2024-02-18 21:27:34 -0500

    Methods such as strip, title and split

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
-#ask user for their name
-name= input("whats ur name? ")
+#ask user for their name strip and capitalize
+name= input("whats ur name? ").strip().title()
+
+#Split users name into first name and last name
+first, last = name.split(" ")
 
 #Say hello to user
-print(f"hello , {name}")
+print(f"hello , {first}")
 
 
 
